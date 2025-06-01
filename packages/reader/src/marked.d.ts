341869fefca151aff4a0807e804cbba8c73d6bfa
Howard Yeh <hayeah@gmail.com>
2025-06-01 09:30:51 +0700

    marked.d.ts

---
+++ packages/reader/src/marked.d.ts
@@ -0,0 +1,22 @@
+declare module 'marked' {
+  export interface MarkedOptions {
+    breaks?: boolean;
+    gfm?: boolean;
+    headerIds?: boolean;
+    mangle?: boolean;
+    pedantic?: boolean;
+    sanitize?: boolean;
+    silent?: boolean;
+    smartLists?: boolean;
+    smartypants?: boolean;
+    xhtml?: boolean;
+  }
+
+  export interface Marked {
+    (markdown: string, options?: MarkedOptions): string;
+    parse: (markdown: string, options?: MarkedOptions) => string;
+    parseInline: (markdown: string, options?: MarkedOptions) => string;
+  }
+
+  export const marked: Marked;
+}
